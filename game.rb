9be423dffad5622f02009c27809ca1e69db6b899
GlauberSCosta <32170356+GlauberSCosta@users.noreply.github.com>
2017-09-28 23:06:29 -0300

    game e test_game
    implementacao e teste:
    update_deaths_by_means
    update_kills_by_means
    subtract_kills
    add_deaths
    add_kills
    add_kill
    players_names
    player

--- game.rb
+++ game.rb
@@ -51,7 +51,51 @@ class Game
     		     	"\t{\n"+
     		     		@players.map{|n,p| "\t\t#{n}"}.join(",\n")+
     		     	"\n\t}\n"+
+    		     	"\tkills:\n"+
+    		     	"\t{\n"+
+    		     		@players.map{|n,p| "\t\t#{n}: #{p.kills} #{p.penalities? ? p.penalities_to_s : ''}"}.join("\n")+
+    		     	"\n\t}\n"+
+			"\tdeaths_by_means:\n"+
+    		     	"\t{\n"+
+    		     		deaths_by_means.map{|n,p| "\t\t#{n}: #{p}"}.join("\n")+
+    		     	"\n\t}\n"+  
     		     "}"
-    	end
+    end
 
+    def player(player_name)
+		begin
+			raise ParserQuakeLogException::PlayerNotExists, "<#{player_name}> Jogador nao faz parte da partida" if !player?(player_name) 
+		@players[player_name]
+		rescue ParserQuakeLogException::PlayerNotExists
+			raise
+		end
+	end
+    
+    def players_names
+		@players.keys.sort
+	end
+
+	def add_kill
+    	@total_kills+=1
+    end
+   
+    def add_kills(player_name)
+		self.player(player_name).add_kills
+	end 	
+	
+	def add_deaths(player_name)
+		self.player(player_name).add_deaths
+	end
+	
+	def subtract_kills(player_name)
+		self.player(player_name).subtract_kills
+	end
+	
+	def update_kills_by_means(player_name, mean)
+		self.player(player_name).update_kills_by_means(mean)
+	end
+	
+	def update_deaths_by_means(player_name, mean)
+		self.player(player_name).update_deaths_by_means(mean)
+	end 
 end
--- test_game.rb
+++ test_game.rb
@@ -15,4 +15,32 @@ class TestGame < Test::Unit::TestCase
 		assert_nothing_raised{match.add_player player1}
 		assert_nothing_raised{match.add_player player2}
 	end
+
+
+	def test_add_and_get_kills
+		p1= Player.new "glauber_costa"
+		p2= Player.new "diluccia_andreza"
+		game = Game.new 1
+
+		game.add_player p1
+		game.add_player p2
+		game.add_kill
+		assert_equal(1, game.total_kills)
+		game.add_kills("glauber_costa")
+		game.add_deaths("diluccia_andreza")
+		assert_equal(1, game.player("diluccia_andreza").deaths)
+		assert_equal(1,game.player("glauber_costa").kills)
+		game.subtract_kills("glauber_costa")
+		assert_equal(0, game.player("glauber_costa").kills)
+		game.update_kills_by_means("glauber_costa", "bereta")
+		assert_equal({"bereta"=> 1}, game.player("glauber_costa").kills_by_means)
+		game.update_deaths_by_means("glauber_costa", "mundo")
+		assert_equal({"mundo"=>1}, game.player("glauber_costa").deaths_by_means)
+
+		assert_raise(ParserQuakeLogException::MustBeString){game.update_kills_by_means("glauber_costa",1)}
+		assert_raise(ParserQuakeLogException::MustBeString){game.update_deaths_by_means("glauber_costa",1)}
+		assert_nothing_raised{game.subtract_kills("glauber_costa")}
+		assert_nothing_raised{game.add_kills("glauber_costa")}
+		
+	end	
 end
